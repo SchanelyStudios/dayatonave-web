0ed653001ea051a5126574b067a09b0a81e47bad
Phil Schanely <phil.schanely@dockyard.com>
2019-05-24 08:26:56 -0400

    Add font awesome and set up header markup

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,41 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <h1>
+  <header class="masthead">
+    <h1 class="masthead__brand">
       <Link to="/">
         {siteTitle}
       </Link>
     </h1>
-    <nav className="nav-main">
+    <div className="masthead__contact-info">
+      <p className="masthead__phone">
+        <span class="fa fa-lg fa-phone"></span> (937) 376-8223
+      </p>
+      <p className="masthead__address">
+        <span class="fa fa-lg fa-map-marker-alt"></span> 1121 Dayton Ave, Xenia, OH 45385
+      </p>
+    </div>
+    <p className="masthead__e-giving">
+      <Link href="https://give.egive-usa.com/dl/?uid=eGive14263Conv">e-Giving</Link>
+    </p>
+    <ul className="masthead__socials">
+      <li>
+        <Link href="https://www.facebook.com/Dayton-Avenue-Baptist-Church-228381720531381/" title="Visit us on Facebook">
+          <span class="fab fa-lg fa-facebook-f"></span>
+        </Link>
+      </li>
+      <li>
+        <Link href="https://www.youtube.com/channel/UCGbFGDEmWDMkEw6dW-K5hhQ" title="View our YouTube channel">
+          <span class="fab fa-lg fa-youtube"></span>
+        </Link>
+      </li>
+      <li>
+        <Link href="https://www.hipcast.com/podcast/H6My90VQ" title="Listen to our Podcase on HipCast">
+          <span class="fa fa-lg fa-podcast"></span>
+        </Link>
+      </li>
+    </ul>
+    <nav className="masthead__nav nav-main">
       <ul className="nav-main__list">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
