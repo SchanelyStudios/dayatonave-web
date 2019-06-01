ede7bcd1e24160f16c160dd1377b7376a89614af
Phil Schanely <philschanely@gmail.com>
2019-06-01 10:31:08 -0400

    0.1.4 -- Media page (#24)
    * Page markup and content
    * Button styles
    * Page stules
    * Increment version

--- src/mocks/media.js
+++ src/mocks/media.js
@@ -2,10 +2,16 @@ import React from "react";
 
 export default {
   title: "Media",
-  content: (
-    <>
-      <h2>Coming soon!</h2>
-      <p>We just can't wait to get this amazing content in place.</p>
-    </>
-  )
+  mediaBlocks: [
+    {
+      heading: "Videos",
+      alias: "video",
+      content: "Our YouTube Channel contains a live feed and video recordings of Sunday morning services and other special events and messages.",
+    },
+    {
+      heading: "Audio",
+      alias: "audio",
+      content: "Our Podcast hosted by HipCast contains a audio recordings of Sunday morning services and other special events and messages.",
+    },
+  ],
 };
