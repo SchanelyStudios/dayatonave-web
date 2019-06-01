c3de204420c4b58e770936665df8498b99919f7a
Phil Schanely <phil.schanely@dockyard.com>
2019-06-01 17:35:02 -0400

    Add prismic integration stub

---
+++ src/utils/linkResolver.js
@@ -0,0 +1,11 @@
+const linkResolver = function(doc){
+  // Pretty URLs for known types
+  // if (doc.type === 'blog') return "/post/" + doc.uid;
+  // if (doc.type === 'page') return "/" + doc.uid;
+  // Fallback for other types, in case new custom types get created
+  return "/doc/" + doc.id;
+}
+
+// const html = RichText.render(document.data.text_field, linkResolver);
+
+export default linkResolver;
