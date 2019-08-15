773a2d2db608caf327923f113215ceafba20f2fa
Phil Schanely <phil.schanely@dockyard.com>
2019-08-14 20:58:20 -0400

    Update media page

--- src/mocks/media.js
+++ src/mocks/media.js
@@ -5,11 +5,15 @@ export default {
       heading: "Videos",
       alias: "video",
       content: "Our YouTube Channel contains a live feed and video recordings of Sunday morning services and other special events and messages.",
+      url: "//youtube.com/channel/UCGbFGDEmWDMkEw6dW-K5hhQ",
+      label: "Watch Now",
     },
     {
       heading: "Audio",
       alias: "audio",
       content: "Our Podcast hosted by HipCast contains a audio recordings of Sunday morning services and other special events and messages.",
+      url: "//hipcast.com/podcast/H6My90VQ",
+      label: "Listen Now",
     },
   ],
 };
