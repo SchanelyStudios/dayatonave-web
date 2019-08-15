7e85a9a67626e32a22f72aee7b8e85ac24461d17
Phil Schanely <phil.schanely@dockyard.com>
2019-08-14 21:38:21 -0400

    Update contact page and styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Placeholdit from "../components/common/placeholdit";
 
 import ContentService from '../services/content.service';
 
@@ -10,9 +11,41 @@ const AboutPage = () => {
   return (
     <Layout activeNavPath="/contact">
       <SEO title={copy.title} />
-      <main className="page">
-        <h1>{copy.title}</h1>
-        {copy.content}
+      <main className="page contact-page">
+        <h2 className="contact-page__title">{copy.title}</h2>
+        <div className="contact-page__map">
+          <Placeholdit size="1200x900" text="Map FPO" />
+        </div>
+        <form className="contact-page__form" action="https://formspree.io/philschanely@gmail.com" method="POST">
+          <ul className="fields">
+            <li className="field">
+              <label className="field__label">Email</label>
+              <div className="field__control">
+                <input type="email" name="_replyto" />
+              </div>
+            </li>
+            <li className="field">
+              <label className="field__label">Subject</label>
+              <div className="field__control">
+                <select name="subject">
+                  <option>General inquiry</option>
+                  <option>Contact senior pastor</option>
+                  <option>Contact executive pastor</option>
+                  <option>Contact youth pastor</option>
+                  <option>Contact children's pastor</option>
+                  <option>Contact worship pastor</option>
+                </select>
+              </div>
+            </li>
+            <li className="field">
+              <label className="field__label">Message</label>
+              <div className="field__control">
+                <textarea name="message"></textarea>
+              </div>
+            </li>
+          </ul>
+          <button type="submit">Send</button>
+        </form>
       </main>
     </Layout>
   );
