fda849d0a5ff34b736c611b3ff2c41e6a77f5708
Phil Schanely <phil.schanely@dockyard.com>
2019-10-01 22:25:08 -0400

    Update content structure

--- src/mocks/about.js
+++ src/mocks/about.js
@@ -72,97 +72,100 @@ export default {
       },
     ],
   },
-  faqs: [
-    {
-      question: 'What are your services like?',
-      answer: (
-        <p>Our services have a definite family atmosphere – people of all ages singing and worshipping together. Attendance in each of our services is around 250 people, so you won’t feel like everyone is watching you, but neither will you feel lost in a crowd. What you will notice is that people are friendly and caring. We enjoy each other’s company, and we enjoy all kinds of music together – from hymns to choruses, from reflective to celebrative, from country gospel to a more contemporary sound. Our preaching is Bible-centered and expositional, with an intentional desire to obey God’s Word and apply it to everyday life. Both Sunday morning services last about 65 minutes. We have a praise ensemble, and a large variety of instruments and special music. The sermon lasts from 30 to 40 minutes, and we always have a time of response at the end. We also have a live stream service at 11:00 in our Youth Room that is a more laid back atmosphere with room to stretch out.</p>
-      ),
-    },
-    {
-      question: 'Where do I park?',
-      answer: (
-        <p>If you have young children, the best place to park is in the back of the church.  Come in the glass doors and follow the hallway to our AVENUE Kids check-in counter.  If you do not have children, or if you need special assistance you can park in our front parking lot.</p>
-      ),
-    },
-    {
-      question: 'Do I have to dress up to attend?',
-      answer: (
-        <p>Though many people dress up somewhat to attend church, we definitely don’t have a dress code. The majority of men do not wear a tie (just in case you were wondering). We want you to come and worship with us, so please don’t let this hold you back from coming.</p>
-      ),
-    },
-    {
-      question: 'Do you use a particular Bible translation?',
-      answer: (
-        <p>Our pastors preach using the New American Standard Bible (1995 update) because it is easy to understand and is a very accurate translation for Bible study. Many of our church family bring other versions to church because they find that it helps them understand the Bible even better when they can compare different translations together.</p>
-      ),
-    },
-    {
-      question: 'Do you embarrass visitors, like make them stand up in the service?',
-      answer: (
-        <p>No we don’t. We do give our visitors a welcome gift but there is nothing to worry about when visiting.</p>
-      ),
-    },
-    {
-      question: 'How does the church support missions?',
-      answer: (
-        <p>In cooperation with the Southern Baptist Convention, we actively support local, national, and international missionaries with financial and prayer support. In addition to supporting missions through the SBC Cooperative Program, Dayton Avenue supports several of our own members who serve with other mission agencies. Next to our church is our Mission Home which we make available to missionaries who have come back to the states for rest and retraining. Finally, we receive special offerings during the year which funnel 100% of the funds directly to the missionaries and to our church family serving in short-term missions. In all, over 20% of our budget goes toward missions.</p>
-      ),
-    },
-    {
-      question: 'I keep hearing about LifeGroups, what are they and how can I get involved?',
-      answer: (
-        <p>We believe it is very important that every member of Dayton Avenue be part of a LifeGroup.  LifeGroups are smaller groups of people that “Do Life” together.  We want to see these groups encompass 4 parts – Bible study – fellowship light (fun/social) – fellowship heavy (support in times of need) and missions. [Link: Read more about our LifeGroups] or [Link: contact our LifeGroup pastor].</p>
-      ),
-    },
-    {
-      question: 'What happens on Wednesday Nights?',
-      answer: (
-        <p>Our mid-week Wednesday night is packed with all sorts of opportunities to connect and grow. These include our weekly prayer meetings, men’s or women’s bible studies, Children’s Ministry and Youth Ministry. We also provide nursery and preschool care for your special little ones!  Bring your family or friends and get involved in a study or ministry.</p>
-      ),
-    },
-    {
-      question: 'Is your church involved in the community?',
-      answer: (
-        <p>Yes! Our church is committed to actively supporting the greater Xenia community. We believe that serving our community is a way of demonstrating the love of Jesus Christ. Many groups such as home schooling groups and support groups use our church facilities which makes Dayton Avenue a community hub. We also participate and support various community ministries that serve in specialized ways. We also support a [Link: free health clinic ministry (http://livingwellclinic.org/)].</p>
-      ),
-    },
-    {
-      question: 'How do I become a member at your church?',
-      answer: (
-        <p>Every 8-10 weeks we host a New Member Information Luncheon on a Sunday afternoon from 12:30–2:45. This informal session is designed to help you learn more about what we believe, how our church functions, and answer any questions you may have—we even provide a free meal! Requirements for membership include placing your trust in Jesus Christ for forgiveness of your sins and baptism as a believer in Jesus Christ. [Link: Read our specific doctrinal positions].</p>
-      ),
-    },
-    {
-      question: 'Do you have a child protection policy?',
-      answer: (
-        <p>We take the safety and security of the children and teens in our church seriously. All children’s workers must be approved through our careful child-protection policy process. Our preschool facilities are monitored to provide security. We will be happy to explain more about this as you check-in and register your child before services. [Link: Learn more about Children’s ministries]</p>
-      ),
-    },
-    {
-      question: 'How are church decisions made at Dayton Avenue?',
-      answer: (
-        <p>Our church is governed by the members of the congregation so decisions are made by their consensus. Committees are appointed to oversee key areas such as personnel, finances, missions, and building and grounds. Committees make recommendations to the church concerning major changes or needs. We currently also have monthly business meetings to conduct routine business and receive reports on the second Wednesday of every month.</p>
-      ),
-    },
-    {
-      question: 'What do your deacons do?',
-      answer: (
-        <p>Our deacons are responsible for serving the needs of the church family. Deacons are not a decision-making body or an executive committee; they are a group of servant-leaders that care for the spiritual and physical needs of Dayton Avenue’s members. Those who are elected to serve are ordained by the church family to the office of deacon and commissioned to serve faithfully and in a manner consistent with their skills and abilities.</p>
+  faqs: {
+    heading: "Frequently Asked Questions",
+    items: [
+      {
+        question: 'What are your services like?',
+        answer: (
+          <p>Our services have a definite family atmosphere – people of all ages singing and worshipping together. Attendance in each of our services is around 250 people, so you won’t feel like everyone is watching you, but neither will you feel lost in a crowd. What you will notice is that people are friendly and caring. We enjoy each other’s company, and we enjoy all kinds of music together – from hymns to choruses, from reflective to celebrative, from country gospel to a more contemporary sound. Our preaching is Bible-centered and expositional, with an intentional desire to obey God’s Word and apply it to everyday life. Both Sunday morning services last about 65 minutes. We have a praise ensemble, and a large variety of instruments and special music. The sermon lasts from 30 to 40 minutes, and we always have a time of response at the end. We also have a live stream service at 11:00 in our Youth Room that is a more laid back atmosphere with room to stretch out.</p>
+        ),
+      },
+      {
+        question: 'Where do I park?',
+        answer: (
+          <p>If you have young children, the best place to park is in the back of the church.  Come in the glass doors and follow the hallway to our AVENUE Kids check-in counter.  If you do not have children, or if you need special assistance you can park in our front parking lot.</p>
+        ),
+      },
+      {
+        question: 'Do I have to dress up to attend?',
+        answer: (
+          <p>Though many people dress up somewhat to attend church, we definitely don’t have a dress code. The majority of men do not wear a tie (just in case you were wondering). We want you to come and worship with us, so please don’t let this hold you back from coming.</p>
+        ),
+      },
+      {
+        question: 'Do you use a particular Bible translation?',
+        answer: (
+          <p>Our pastors preach using the New American Standard Bible (1995 update) because it is easy to understand and is a very accurate translation for Bible study. Many of our church family bring other versions to church because they find that it helps them understand the Bible even better when they can compare different translations together.</p>
+        ),
+      },
+      {
+        question: 'Do you embarrass visitors, like make them stand up in the service?',
+        answer: (
+          <p>No we don’t. We do give our visitors a welcome gift but there is nothing to worry about when visiting.</p>
+        ),
+      },
+      {
+        question: 'How does the church support missions?',
+        answer: (
+          <p>In cooperation with the Southern Baptist Convention, we actively support local, national, and international missionaries with financial and prayer support. In addition to supporting missions through the SBC Cooperative Program, Dayton Avenue supports several of our own members who serve with other mission agencies. Next to our church is our Mission Home which we make available to missionaries who have come back to the states for rest and retraining. Finally, we receive special offerings during the year which funnel 100% of the funds directly to the missionaries and to our church family serving in short-term missions. In all, over 20% of our budget goes toward missions.</p>
+        ),
+      },
+      {
+        question: 'I keep hearing about LifeGroups, what are they and how can I get involved?',
+        answer: (
+          <p>We believe it is very important that every member of Dayton Avenue be part of a LifeGroup.  LifeGroups are smaller groups of people that “Do Life” together.  We want to see these groups encompass 4 parts – Bible study – fellowship light (fun/social) – fellowship heavy (support in times of need) and missions. [Link: Read more about our LifeGroups] or [Link: contact our LifeGroup pastor].</p>
+        ),
+      },
+      {
+        question: 'What happens on Wednesday Nights?',
+        answer: (
+          <p>Our mid-week Wednesday night is packed with all sorts of opportunities to connect and grow. These include our weekly prayer meetings, men’s or women’s bible studies, Children’s Ministry and Youth Ministry. We also provide nursery and preschool care for your special little ones!  Bring your family or friends and get involved in a study or ministry.</p>
+        ),
+      },
+      {
+        question: 'Is your church involved in the community?',
+        answer: (
+          <p>Yes! Our church is committed to actively supporting the greater Xenia community. We believe that serving our community is a way of demonstrating the love of Jesus Christ. Many groups such as home schooling groups and support groups use our church facilities which makes Dayton Avenue a community hub. We also participate and support various community ministries that serve in specialized ways. We also support a [Link: free health clinic ministry (http://livingwellclinic.org/)].</p>
+        ),
+      },
+      {
+        question: 'How do I become a member at your church?',
+        answer: (
+          <p>Every 8-10 weeks we host a New Member Information Luncheon on a Sunday afternoon from 12:30–2:45. This informal session is designed to help you learn more about what we believe, how our church functions, and answer any questions you may have—we even provide a free meal! Requirements for membership include placing your trust in Jesus Christ for forgiveness of your sins and baptism as a believer in Jesus Christ. [Link: Read our specific doctrinal positions].</p>
+        ),
+      },
+      {
+        question: 'Do you have a child protection policy?',
+        answer: (
+          <p>We take the safety and security of the children and teens in our church seriously. All children’s workers must be approved through our careful child-protection policy process. Our preschool facilities are monitored to provide security. We will be happy to explain more about this as you check-in and register your child before services. [Link: Learn more about Children’s ministries]</p>
+        ),
+      },
+      {
+        question: 'How are church decisions made at Dayton Avenue?',
+        answer: (
+          <p>Our church is governed by the members of the congregation so decisions are made by their consensus. Committees are appointed to oversee key areas such as personnel, finances, missions, and building and grounds. Committees make recommendations to the church concerning major changes or needs. We currently also have monthly business meetings to conduct routine business and receive reports on the second Wednesday of every month.</p>
+        ),
+      },
+      {
+        question: 'What do your deacons do?',
+        answer: (
+          <p>Our deacons are responsible for serving the needs of the church family. Deacons are not a decision-making body or an executive committee; they are a group of servant-leaders that care for the spiritual and physical needs of Dayton Avenue’s members. Those who are elected to serve are ordained by the church family to the office of deacon and commissioned to serve faithfully and in a manner consistent with their skills and abilities.</p>
 
-      ),
-    },
-    {
-      question: 'How can I get involved at Dayton Avenue?',
-      answer: (
-        <p>There is a wide variety of ministries in which you can serve, spanning from preschool and children’s ministry to music and prayer ministries. We believe that each believer is given gifts to use for the encouragement and edification of the church family and thus we encourage each one to find a place to serve. Learn more by [Llink: completing an involvement form] or talking to any of our pastors.</p>
-      ),
-    },
-    {
-      question: 'Do you have a church constitution?',
-      answer: (
-        <p>Yes! [Link: Read our church constitution], last updated in 2014.</p>
-      ),
-    }
-  ]
+        ),
+      },
+      {
+        question: 'How can I get involved at Dayton Avenue?',
+        answer: (
+          <p>There is a wide variety of ministries in which you can serve, spanning from preschool and children’s ministry to music and prayer ministries. We believe that each believer is given gifts to use for the encouragement and edification of the church family and thus we encourage each one to find a place to serve. Learn more by [Llink: completing an involvement form] or talking to any of our pastors.</p>
+        ),
+      },
+      {
+        question: 'Do you have a church constitution?',
+        answer: (
+          <p>Yes! [Link: Read our church constitution], last updated in 2014.</p>
+        ),
+      }
+    ]
+  }
 };
