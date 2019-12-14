57f24f2d7e75920867ea68a636b6e10200de2be3
Phil Schanely <phil.schanely@dockyard.com>
2019-12-14 11:48:52 -0500

    Patch errors

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,32 +7,32 @@ import Logo from "./common/logo";
 import FooterConfig from "../config/footer.config";
 
 const Footer = ({ siteTitle }) => (
-  <footer class="footer">
+  <footer className="footer">
   	<Logo type="wordmark" color="mono-dark">
       {siteTitle}
     </Logo>
 
-  	<nav class="footer__members">
-      <Link class="footer__members__cta btn btn--secondary"
+  	<nav className="footer__members">
+      <Link className="footer__members__cta btn btn--secondary"
         to={FooterConfig.members.path}
       >
         {FooterConfig.members.label}
       </Link>
-  		<p class="footer__members__description">
+  		<p className="footer__members__description">
         {FooterConfig.members.description}
       </p>
   	</nav>
 
-  	<div class="footer__socials">
-  		<h2 class="visually-hidden">Media and Social Links</h2>
-  		<ul class="nav nav--footer-socials">
+  	<div className="footer__socials">
+  		<h2 className="visually-hidden">Media and Social Links</h2>
+  		<ul className="nav nav--footer-socials">
   			{FooterConfig.socials.map((item, j) => {
           return (
             <li className="nav__item" key={j}>
               <a className={`nav__link default icon icon--${item.iconClasses}`}
                 href={item.url}
               >
-                <span class="visually-hidden">
+                <span className="visually-hidden">
                   {item.label}
                 </span>
               </a>
@@ -42,7 +42,7 @@ const Footer = ({ siteTitle }) => (
   		</ul>
   	</div>
 
-  	<div class="footer__privacy-terms">
+  	<div className="footer__privacy-terms">
   		<p>
         <Link to={FooterConfig.terms.path}>
           {FooterConfig.terms.label}
@@ -50,7 +50,7 @@ const Footer = ({ siteTitle }) => (
       </p>
   	</div>
 
-  	<div class="footer__copyright">
+  	<div className="footer__copyright">
   		{FooterConfig.copyright}
   	</div>
   </footer>
