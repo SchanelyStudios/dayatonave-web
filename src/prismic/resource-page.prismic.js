6101752a196e5735a33e3b2570fecb27555f24a4
Phil Schanely <philschanely@gmail.com>
2020-02-08 17:33:14 -0500

    Resource page fallback

--- src/prismic/resource-page.prismic.js
+++ src/prismic/resource-page.prismic.js
@@ -3,6 +3,15 @@ import { renderText, renderHtml, resolveImage } from "../utils/prismicRenderer";
 export default (input) => {
   let page = input.prismic.resource_page;
 
+  if (!page) {
+    return {
+      title: "Error",
+      description: "The page could not be found.",
+      link: null,
+      cover_image: null
+    }
+  }
+
   return {
     title: renderText(page.resource_name),
     description: renderHtml(page.description),
